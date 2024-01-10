db6dc248127d24ffae530e5c7c3b2606d5e70761
muslinazavtrak@gmail.com <muslinazavtrak@gmail.com>
2024-01-11 00:46:57 +0300

    Добавьте файлы проекта.

---
+++ ADO_NET_ДЗ_Модуль_02_часть_01/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ADO_NET_ДЗ_Модуль_02_часть_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_execute = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_choice = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 285);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(457, 214);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btn_execute
+            // 
+            this.btn_execute.Location = new System.Drawing.Point(102, 399);
+            this.btn_execute.Name = "btn_execute";
+            this.btn_execute.Size = new System.Drawing.Size(75, 23);
+            this.btn_execute.TabIndex = 2;
+            this.btn_execute.Text = "Inserrt";
+            this.btn_execute.UseVisualStyleBackColor = true;
+            this.btn_execute.Click += new System.EventHandler(this.btn_execute_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btn_choice);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(536, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 145);
+            this.panel1.TabIndex = 3;
+            // 
+            // btn_choice
+            // 
+            this.btn_choice.Location = new System.Drawing.Point(125, 30);
+            this.btn_choice.Name = "btn_choice";
+            this.btn_choice.Size = new System.Drawing.Size(75, 64);
+            this.btn_choice.TabIndex = 2;
+            this.btn_choice.Text = "OK";
+            this.btn_choice.UseVisualStyleBackColor = true;
+            this.btn_choice.Click += new System.EventHandler(this.btn_choice_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(33, 77);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 17);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Category";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(33, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Providers";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(33, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Products";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(177, 322);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(177, 359);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Category";
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(202, 399);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 2;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(327, 311);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 2;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ID";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(348, 285);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(54, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_execute);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "StorageMeneger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_execute;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btn_choice;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ADO_NET_ДЗ_Модуль_02_часть_01/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace ADO_NET_ДЗ_Модуль_02_часть_01
+{
+    public partial class Form1 : Form
+    {
+        private string connection_string;
+        private NpgsqlConnection connection;
+        private string table_name;
+        public Form1()
+        {
+            InitializeComponent();
+            connection_string = "Host=localhost;" +
+                "Username=postgres;" +
+                "Password=123;" +
+                "Database=warehouse;";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new NpgsqlConnection(connection_string);
+            connection.Open();
+           
+        }
+        private DataTable loadTable()
+        {
+            DataTable storage_dt = new DataTable();
+            NpgsqlDataAdapter sda = new NpgsqlDataAdapter($"SELECT * FROM {table_name}", connection);
+            sda.Fill(storage_dt);
+            return storage_dt;
+        }
+        
+        private void btn_execute_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) insertToProducts();
+            if (radioButton2.Checked) insertToProviders();
+            if (radioButton3.Checked) insertToCategory();
+        }
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) updateToProducts();
+            if (radioButton2.Checked) updateToProviders();
+            if (radioButton3.Checked) updateToCategory();
+        }
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) DeleteData();
+            if (radioButton2.Checked) DeleteData();
+            if (radioButton3.Checked) DeleteData();
+        }
+
+        private void insertToProducts()
+        {
+            string commandText = $"INSERT INTO {table_name} (_name, _price, category_id)" +
+               $" VALUES (@name,@price,@category_id)";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("name", textBox1.Text);
+                cmd.Parameters.AddWithValue("price", Convert.ToDecimal(textBox2.Text));
+                cmd.Parameters.AddWithValue("category_id", Convert.ToInt32(textBox3.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data has been inserted successfully!");
+                dataGridView1.DataSource = loadTable();
+            }
+        }
+        private void insertToProviders()
+        {
+            string commandText = $"INSERT INTO {table_name} (_name, _phone_number)" +
+               $" VALUES (@name,@phone_number)";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("name", textBox1.Text);
+                cmd.Parameters.AddWithValue("phone_number", Convert.ToInt64(textBox2.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data has been inserted successfully!");
+                dataGridView1.DataSource = loadTable();
+            }
+        }
+
+        private void insertToCategory()
+        {
+            string commandText = $"INSERT INTO {table_name} (_name)" +
+               $" VALUES (@name)";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("name", textBox1.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data has been inserted successfully!");
+                dataGridView1.DataSource = loadTable();
+            }
+        }
+        private void updateToProducts()
+        {
+            string commandText = $"UPDATE {table_name} SET _name = @name, _price = @price, category_id = @category_id" +
+                $" WHERE id = @id";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("id", Convert.ToInt16(textBox4.Text));
+                cmd.Parameters.AddWithValue("name", textBox1.Text);
+                cmd.Parameters.AddWithValue("price", Convert.ToDecimal(textBox2.Text));
+                cmd.Parameters.AddWithValue("category_id", Convert.ToInt32(textBox3.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data has been updated successfully!");
+                dataGridView1.DataSource = loadTable();
+            }
+        }
+        private void updateToProviders()
+        {
+            string commandText = $"UPDATE {table_name} SET _name = @name, _phone_number = @phone_number" +
+                $" WHERE id = @id";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("id", Convert.ToInt16(textBox4.Text));
+                cmd.Parameters.AddWithValue("name", textBox1.Text);
+                cmd.Parameters.AddWithValue("phone_number", Convert.ToInt64(textBox2.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data has been updated successfully!");
+                dataGridView1.DataSource = loadTable();
+            }
+        }
+        private void updateToCategory()
+        {
+            string commandText = $"UPDATE {table_name} SET _name = @name WHERE id = @id";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("id", Convert.ToInt16(textBox4.Text));
+                cmd.Parameters.AddWithValue("name", textBox1.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data has been updated successfully!");
+                dataGridView1.DataSource = loadTable();
+            }
+        }
+
+        private void DeleteData()
+        {
+            string commandText = $"DELETE FROM {table_name} WHERE id = @id";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("id", Convert.ToInt16(textBox4.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data has been deleted successfully!");
+                dataGridView1.DataSource = loadTable();
+            }
+        }
+
+        private void btn_choice_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                ClearAllText();
+                table_name = "products";
+                dataGridView1.DataSource = loadTable();
+                label1.Text = "Name product";
+                textBox1.Enabled = true;
+                label2.Text = "Price";
+                textBox2.Enabled = true;
+                label3.Text = "Category";
+                textBox3.Enabled = true;
+                
+            }
+            if(radioButton2.Checked)
+            {
+                ClearAllText();
+                table_name = "providers";
+                dataGridView1.DataSource = loadTable();
+                label1.Text = "Name provider";
+                textBox1.Enabled = true;
+                label2.Text = "Phone number";
+                textBox2.Enabled = true;
+                label3.Text = "";
+                textBox3.Enabled = false;
+                
+            }
+            if(radioButton3.Checked)
+            {
+                ClearAllText();
+                table_name = "category_product";
+                dataGridView1.DataSource = loadTable();
+                label1.Text = "Category Name";
+                textBox1.Enabled = true;
+                label2.Text = "";
+                textBox2.Enabled = false;
+                label3.Text = "";
+                textBox3.Enabled = false;
+                
+            }
+        }
+        private void ClearAllText()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+       
+    }
+    
+}
